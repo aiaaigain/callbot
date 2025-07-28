636a0c49d4ed27b87134309747cc8d7a08f3583a
aiaaigain <aigainaigain25@gmail.com>
2025-07-28 17:44:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('🟢 Agente Recepcionista funcionando');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
 
